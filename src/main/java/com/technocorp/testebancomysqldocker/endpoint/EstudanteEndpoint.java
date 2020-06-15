da3edc26a7e4dcbd9e44c6893007bf243c130939
roger <roger.lucian>
2020-06-14 21:26:48 -0300

    configurado os endpoints.

--- src/main/java/com/technocorp/testebancomysqldocker/endpoint/EstudanteEndpoint.java
+++ src/main/java/com/technocorp/testebancomysqldocker/endpoint/EstudanteEndpoint.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("estudantes")
 public class EstudanteEndpoint {
-
+//blble
     private final DateUtil dateUtil;
     @Autowired
     public EstudanteEndpoint(DateUtil dateUtil) {
